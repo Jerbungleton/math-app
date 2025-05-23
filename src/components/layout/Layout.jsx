4e53802cd884a94c85217a2a0138014f3bd4f98b
Jabonga <davidjaythanruiz12@gmail.com>
2025-05-22 19:30:15 -0500

    s

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,7 +5,7 @@ export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen bg-brand-background">
       <Navbar />
-      <main className="flex-grow w-full mx-auto px-8 py-8">
+      <main className="flex-grow w-full max-w-[1200px] mx-auto px-8 py-8">
         <Outlet />
       </main>
       {/* Optional: Add a Footer component here, styled with new brand colors */}
