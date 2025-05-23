bfdabe1b086a98b52cedf73d2de65fb0a365127b
Jabonga <davidjaythanruiz12@gmail.com>
2025-05-22 19:19:25 -0500

    ye

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,22 +1,17 @@
+import mathAnimation from '../assets/animations/math-animation.gif';
+
 export default function AboutPage() {
   return (
     <div className="container mx-auto p-8">
-      <h1 className="text-4xl font-bold mb-6 text-center text-brand-primary-dark">sup dawg</h1> {/* */}
-      <div className="content-card text-brand-text-dark"> {/* */}
-        <p className="mb-4 text-lg">
-          Filler Text 
-          Filler Text].
-        </p>
-        <p className="mb-4 text-lg">
-          Filler Text 
-          Filler Text
-        </p>
-        <p className="text-lg">
-          Music {/* Consider changing this if the site is no longer about music */}
-        </p>
-        <p className="mt-6 text-lg">
-          Filler
-        </p>
+      <h1 className="text-4xl font-bold mb-6 text-center text-brand-primary-dark">learn math son</h1>
+      <div className="content-card text-brand-text-dark">
+        <div className="flex justify-center mb-8">
+          <img 
+            src={mathAnimation} 
+            alt="Math Animation" 
+            className="rounded-lg shadow-lg w-[600px] h-auto"
+          />
+        </div>
       </div>
     </div>
   );
